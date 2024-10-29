9b03b117fa6ff15afed44a9a4aa3ce0f86bdb5e6
Bastien <po77qxd@eduvaud.ch>
2024-10-29 14:23:25 +0100

    add ex UML

---
+++ BastienSegalen/testUML/exUML/exUML/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace exUML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Car
+    {
+        public int speed;
+        private string _color;
+        private string _modele;
+        private List<Passenger> passengers = new List<Passenger>();
+        public Car(string color, string modele)
+        {
+            _color = color;
+            _modele = modele;
+        }
+        public void AddPassenger(Passenger passenger)
+        {
+            passengers.Add(passenger);
+        }
+        public void start()
+        {
+
+        }
+        public void stop()
+        {
+
+        }
+
+    }
+    public class Passenger
+    {
+        private string _name;
+    }
+}
